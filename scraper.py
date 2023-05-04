7c410ffc244536cdd7fbee194aa6d1d9a5e00f0f
mirayagupta13 <124533495+mirayagupta13@users.noreply.github.com>
2023-05-04 19:13:30 -0400

    Add files via upload

---
+++ scraper.py
@@ -0,0 +1,14 @@
+"""
+Authors: Jenna, Miraya, Jaci
+Title: Sexism on the Silver Screen
+"""
+
+import requests
+from bs4 import BeautifulSoup
+import wikipedia
+ 
+list_of_actors =
+all_pages_text = []
+for actor in list_of_actors:
+    page = wikipedia.page(actor)
+    all_pages_text.append(page.text)
