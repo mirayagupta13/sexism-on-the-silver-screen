759c8e5d2a05a2750319ef81bc0c831d00208cc4
mirayagupta13 <124533495+mirayagupta13@users.noreply.github.com>
2023-05-08 21:12:09 -0400

    Update dataclasses_1.py

--- dataclasses_1.py
+++ dataclasses_1.py
@@ -68,7 +68,7 @@ for item in directory.iterdir():
             STOPWORDS = {'i', 'me', 'my', 'myself', 'we', 'our', 'ours', 'ourselves', 'you', 'your', 'yours', 'yourself', 'yourselves', 'he', 'him', 'his', 'himself', 'she', 'her', 'hers', 'herself', 'it', 'its', 'itself', 'they', 'them', 'their', 'theirs', 'themselves', 'what', 'which', 'who', 'whom', 'this', 'that', 'these', 'those', 'am', 'is', 'are', 'was', 'were', 'be', 'been', 'being', 'have', 'has', 'had', 'having', 'do', 'does', 'did', 'doing', 'a', 'an', 'the', 'and', 'but', 'if', 'or', 'because', 'as', 'until', 'while', 'of', 'at', 'by', 'for', 'with', 'about', 'against', 'between', 'into', 'through', 'during', 'before', 'after', 'above', 'below', 'to', 'from', 'up', 'down', 'in', 'out', 'on', 'off', 'over', 'under', 'again', 'further', 'then', 'once', 'here', 'there', 'when', 'where', 'why', 'how', 'all', 'any', 'both', 'each', 'few', 'more', 'most', 'other', 'some', 'such', 'no', 'nor', 'not', 'only', 'own', 'same', 'so', 'than', 'too', 'very', 's', 't', 'can', 'will', 'just', 'don', 'should', 'now'}
 
             nltk_o = nltk.word_tokenize(content)
-            nltk_o = [word for word in nltk_o if word not in STOPWORDS]
+            nltk_o = [word for word in nltk_o if word.lower() not in STOPWORDS]
             
             actor_list.append(actor(a_name, a_gender, content, spacy_o, nltk_o))
             nltk_o = nltk.word_tokenize(content)
